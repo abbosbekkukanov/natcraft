744c55934e235ef9dbce7493b51f30e29bf16861
abbosbekkukanov <abbosbekkukanov500@gmail.com>
2025-04-07 14:24:49 +0500

    Products APPi takomillashtirildi Filter, Pagination, Search qo'shildi 07.04.2025

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'django_filters',
     'products',
     'accounts',
     'main',
@@ -37,7 +38,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+    ],
 }
 
 AUTHENTICATION_BACKENDS = (
