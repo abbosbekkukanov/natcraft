a284f6c97f65b435a2526596669bfaa45707765d
abbosbekkukanov <abbosbekkukanov500@gmail.com>
2025-04-08 21:20:08 +0500

    Chat views.py updated 08.04.2025

--- chat/models.py
+++ chat/models.py
@@ -25,6 +25,7 @@ class Message(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_read = models.BooleanField(default=False)
+    is_edited = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Message from {self.sender.email} in {self.chat}"
