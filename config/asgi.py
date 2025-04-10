021655a63e55751aa162a931d554dd6299e2c7c1
abbosbekkukanov <abbosbekkukanov500@gmail.com>
2025-04-10 19:15:27 +0500

    WebSocket Add Chat 10.04.2025

--- config/asgi.py
+++ config/asgi.py
@@ -1,4 +1,5 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -6,6 +7,7 @@ from chat.middleware import TokenAuthMiddleware
 import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
 
 django_asgi_app = get_asgi_application()
 
