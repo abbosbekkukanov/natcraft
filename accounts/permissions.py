b9a1acf1223a99400eb7690f1bbdba26e8ddf48e
abbosbekkukanov <abbosbekkukanov500@gmail.com>
2025-04-16 20:36:18 +0500

    profession url 16.04.2025

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -3,8 +3,15 @@ from rest_framework import permissions
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     """
-    Foydalanuvchi faqat o'zini yaratgan ma'lumotni o'zgartirishi mumkin.
+    Hamma ko‘rishi mumkin (GET), faqat ro‘yxatdan o‘tgan foydalanuvchi post, put, delete qila oladi.
+    PUT va DELETE esa faqat o'z ma'lumotiga.
     """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
